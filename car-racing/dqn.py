b3f74cd3470553cdf8da5489c014a9c3172fa18a
Daniel Barbosa <daniel@Mercurio.local>
2022-08-31 14:37:46 -0400

    noop reset

--- car-racing/dqn.py
+++ car-racing/dqn.py
@@ -114,6 +114,38 @@ class SkipFrame(gym.Wrapper):
         return obs, total_reward, done, info
 
 
+class NoopResetEnv(gym.Wrapper):
+
+    def __init__(self, env, noop_max=30):
+        """Sample initial states by taking random number of no-ops on reset.
+        No-op is assumed to be action 0.
+        """
+        gym.Wrapper.__init__(self, env)
+        self.noop_max = noop_max
+        self.override_num_noops = None
+        self.noop_action = 0
+        #assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
+
+    def reset(self, **kwargs):
+        """ Do no-op action for a number of steps in [1, noop_max]."""
+        self.env.reset(**kwargs)
+        if self.override_num_noops is not None:
+            noops = self.override_num_noops
+        else:
+            noops = self.unwrapped.np_random.randint(1, self.noop_max + 1)  # pylint: disable=E1101
+        assert noops > 0
+        print('noops:', noops)
+        obs = None
+        for _ in range(noops):
+            obs, _, done, _ = self.env.step(self.noop_action)
+            if done:
+                obs = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, ac):
+        return self.env.step(ac)
+
+
 class Model2Layer(nn.Module):
     """
     Input is 4 stacked 84x84 int8 grayscale frames.
